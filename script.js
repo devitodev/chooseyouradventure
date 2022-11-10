061857c6314322813aec9c3462006d02bba82f33
devitodev <softwaredev.ito@gmail.com>
2022-11-10 13:45:54 -0600

    Added intial code from work cpu

---
+++ script.js
@@ -0,0 +1,10 @@
+var story = document.querySelectorAll('.story');
+console.log(story)
+ 
+story.addEventListener('click', storyDisappear)
+ 
+function storyDisappear() {
+ if (storyDisappear = story.addEventListener) {
+     story = story.style.red
+ }
+}
